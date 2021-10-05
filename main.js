37efa1ccc487d6ea6cb6616a625420ddf3b7aba7
Tfronn <keeelvincamilo@hotmail.com>
2021-10-05 18:34:50 -0300

    versao atual

---
+++ main.js
@@ -0,0 +1,57 @@
+// abre e fecha menu
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+// clicar em item do menu-esconder
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+//mudar sombra header ao scroll
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
+
+//testimonials slider
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* scroll reveal */
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal('.image')
+scrollReveal.reveal('.text', { delay: 100 }),
+  scrollReveal.reveal('.card', { interval: 200 }),
+  scrollReveal.reveal('.testimonials')
+scrollReveal.reveal('.links', { interval: 200 })
+scrollReveal.reveal('header')
